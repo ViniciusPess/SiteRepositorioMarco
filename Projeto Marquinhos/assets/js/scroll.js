e518a354ac399c0f8e8386253c6202dc1a12ae3b
Vinicius Pessanha <113379730+ViniciusPess@users.noreply.github.com>
2023-09-22 14:58:37 -0300

    Update scroll.js

--- Projeto Marquinhos/assets/js/scroll.js
+++ Projeto Marquinhos/assets/js/scroll.js
@@ -10,3 +10,21 @@ sr.reveal('.project', {duration : 3000, origin:'bottom'})
 sr.reveal('.github-link', {duration : 3000, origin:'bottom'})
 sr.reveal('.conteiner-skills', {duration : 3000, origin:'left'})
 sr.reveal('.conteiner-descricao', {duration : 3000, origin:'right'})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const barra = document.querySelector(".barra");
+    const barraHeight = barra.clientHeight;
+    let lastScrollPosition = 0;
+
+    window.addEventListener("scroll", function() {
+        const scrollPosition = window.scrollY;
+
+        if (scrollPosition > lastScrollPosition) {
+            barra.style.transform = `translateY(-${barraHeight}px)`;
+        } else {
+            barra.style.transform = "translateY(0)";
+        }
+
+        lastScrollPosition = scrollPosition;
+    });
+});
